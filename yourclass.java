b70f6c8ac39161b35adfbb778d469ff8a8008f2e
PavelSynek <pavel.synek@gmail.com>
2016-03-16 17:45:18 +0100

    reformat

--- yourclass.java
+++ yourclass.java
@@ -1,8 +1,3 @@
-import java.awt.BasicStroke;
-import java.awt.Color;
-import java.awt.Graphics2D;
-import java.awt.Stroke;
-import java.awt.Window;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.event.MouseEvent;
@@ -38,119 +33,119 @@ public class yourclass extends Core implements KeyListener, MouseListener,
 	}
 
 	public void draw(Graphics2D g) {
-		switch(currentDirection1){
-		case 0:
-			if (centrey1>0){
-			centrey1-=moveAmount;
-			} else {
-				centrey1 = sm.getHeight();
-			}
-			break;
-		case 1:
-			if (centrex1 < sm.getWidth()){
-			centrex1+=moveAmount;
-			} else {
-				centrex1 = 0;
-			}
-			break;
-		case 2:
-			if (centrey1 < sm.getHeight()){
-			centrey1+=moveAmount;
-			} else {
-				centrey1 = 0;
-			}
-			break;
-		case 3:
-			if (centrex1>0){
-			centrex1-=moveAmount;
-			} else {
-				centrex1 = sm.getWidth();
-			}
-			break;
+		switch (currentDirection1) {
+			case 0:
+				if (centrey1 > 0) {
+					centrey1 -= moveAmount;
+				} else {
+					centrey1 = sm.getHeight();
+				}
+				break;
+			case 1:
+				if (centrex1 < sm.getWidth()) {
+					centrex1 += moveAmount;
+				} else {
+					centrex1 = 0;
+				}
+				break;
+			case 2:
+				if (centrey1 < sm.getHeight()) {
+					centrey1 += moveAmount;
+				} else {
+					centrey1 = 0;
+				}
+				break;
+			case 3:
+				if (centrex1 > 0) {
+					centrex1 -= moveAmount;
+				} else {
+					centrex1 = sm.getWidth();
+				}
+				break;
 		}
-		switch(currentDirection2){
-		case 0:
-			if (centrey2>0){
-			centrey2-=moveAmount;
-			} else {
-				centrey2 = sm.getHeight();
-			}
-			break;
-		case 1:
-			if (centrex2 < sm.getWidth()){
-			centrex2+=moveAmount;
-			} else {
-				centrex2 = 0;
-			}
-			break;
-		case 2:
-			if (centrey2 < sm.getHeight()){
-			centrey2+=moveAmount;
-			} else {
-				centrey2 = 0;
-			}
-			break;
-		case 3:
-			if (centrex2>0){
-			centrex2-=moveAmount;
-			} else {
-				centrex2 = sm.getWidth();
-			}
-			break;
+		switch (currentDirection2) {
+			case 0:
+				if (centrey2 > 0) {
+					centrey2 -= moveAmount;
+				} else {
+					centrey2 = sm.getHeight();
+				}
+				break;
+			case 1:
+				if (centrex2 < sm.getWidth()) {
+					centrex2 += moveAmount;
+				} else {
+					centrex2 = 0;
+				}
+				break;
+			case 2:
+				if (centrey2 < sm.getHeight()) {
+					centrey2 += moveAmount;
+				} else {
+					centrey2 = 0;
+				}
+				break;
+			case 3:
+				if (centrex2 > 0) {
+					centrex2 -= moveAmount;
+				} else {
+					centrex2 = sm.getWidth();
+				}
+				break;
 		}
-	    for (int x = 0;x<pathx1.size();x++){
-	    	if (((centrex1 == pathx1.get(x)) && (centrey1 == pathy1.get(x))) || ((centrex2 == pathx2.get(x)) && (centrey2 == pathy2.get(x))) || ((centrex1 == pathx2.get(x)) && (centrey1 == pathy2.get(x))) || ((centrex2 == pathx1.get(x)) && (centrey2 == pathy1.get(x)))){
-	    		System.exit(0);
-	    	}
-	    }
+		for (int x = 0; x < pathx1.size(); x++) {
+			if (((centrex1 == pathx1.get(x)) && (centrey1 == pathy1.get(x))) || ((centrex2 == pathx2.get(x)) && (centrey2 == pathy2.get(x))) || ((centrex1 == pathx2.get(x)) && (centrey1 == pathy2.get(x))) || ((centrex2 == pathx1.get(x)) && (centrey2 == pathy1.get(x)))) {
+				System.exit(0);
+			}
+		}
 		pathx1.add(centrex1);
 		pathy1.add(centrey1);
 		pathx2.add(centrex2);
 		pathy2.add(centrey2);
 		g.setColor(Color.BLACK);
 		g.fillRect(0, 0, sm.getWidth(), sm.getHeight());
-		for (int x = 0;x<pathx1.size();x++){
-		g.setColor(Color.green);
-		g.fillRect(pathx1.get(x), pathy1.get(x), 10, 10);
-		g.setColor(Color.red);
-		g.fillRect(pathx2.get(x), pathy2.get(x), 10, 10);
+		for (int x = 0; x < pathx1.size(); x++) {
+			g.setColor(Color.green);
+			g.fillRect(pathx1.get(x), pathy1.get(x), 10, 10);
+			g.setColor(Color.red);
+			g.fillRect(pathx2.get(x), pathy2.get(x), 10, 10);
 		}
 	}
 
 	public void keyPressed(KeyEvent e) {
 		if (e.getKeyCode() == KeyEvent.VK_UP) {
-			if (currentDirection1 != 2){
-			currentDirection1 = 0;
+			if (currentDirection1 != 2) {
+				currentDirection1 = 0;
 			}
 		} else if (e.getKeyCode() == KeyEvent.VK_DOWN) {
-			if (currentDirection1 != 0){
+			if (currentDirection1 != 0) {
 				currentDirection1 = 2;
-				}
+			}
 		} else if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
-			if (currentDirection1 != 3){
+			if (currentDirection1 != 3) {
 				currentDirection1 = 1;
-				}
+			}
 		} else if (e.getKeyCode() == KeyEvent.VK_LEFT) {
-			if (currentDirection1 != 1){
+			if (currentDirection1 != 1) {
 				currentDirection1 = 3;
-				}
+			}
 		}
-		if (e.getKeyCode() == KeyEvent.VK_W){
-			if (currentDirection2 != 2){
-			currentDirection2 = 0;
+		if (e.getKeyCode() == KeyEvent.VK_W) {
+			if (currentDirection2 != 2) {
+				currentDirection2 = 0;
 			}
 		} else if (e.getKeyCode() == KeyEvent.VK_S) {
-			if (currentDirection2 != 0){
+			if (currentDirection2 != 0) {
 				currentDirection2 = 2;
-				}
+			}
 		} else if (e.getKeyCode() == KeyEvent.VK_D) {
-			if (currentDirection2 != 3){
+			if (currentDirection2 != 3) {
 				currentDirection2 = 1;
-				}
+			}
 		} else if (e.getKeyCode() == KeyEvent.VK_A) {
-			if (currentDirection2 != 1){
+			if (currentDirection2 != 1) {
 				currentDirection2 = 3;
-				}
+			}
 		}
 	}
 
